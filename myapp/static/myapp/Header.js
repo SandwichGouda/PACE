f5e58340636eb9b2d7653343f3937f9b526f3c8d
SandwichGouda <william.driot@gmail.com>
2024-09-30 00:24:25 +0200

    Header nice shade of blue.

---
+++ myapp/static/myapp/Header.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const accountBtn = document.querySelector('.account-btn');
+    
+    accountBtn.addEventListener('click', function() {
+        alert('Account button clicked!');
+        // You can replace this with your desired functionality
+    });
+});
