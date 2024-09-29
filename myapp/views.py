4cabc5f926dc91d243b2d1130fcc11e68f6aa873
SandwichGouda <william.driot@gmail.com>
2024-09-30 01:34:47 +0200

    Front page finished, only buttons remaining. Account button redirects to admin.html

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def admin(request):
+    return render(request, 'admin.html')
+
+def pace2022(request):
+    return render(request, 'pace2022.html')
+
+def pace2023(request):
+    return render(request, 'pace2023.html')
+
+def pace2024(request):
+    return render(request, 'pace2024.html')
