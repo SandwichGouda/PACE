cd1b0dc81006bae90e0ae94649492b8dd3574011
SandwichGouda <william.driot@gmail.com>
2024-10-31 23:50:21 +0100

    Able to serve PDFs.

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin', views.admin, name='admin'),
     path('pace/2023', views.pace2023, name='PACE2023'),
+    path('pdfs/<str:filename>/', views.serve_pdf, name='serve_pdf'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import FileResponse, Http404, HttpResponseRedirect
+import os
+from django.conf import settings
+from django.contrib.staticfiles import finders
 
 # Create your views here.
 
@@ -10,3 +14,29 @@ def admin(request):
 
 def pace2023(request):
     return render(request, 'PACE2023.html')
+
+def serve_pdf(request, filename):
+    # Construct the full file path
+    file_path = os.path.join(settings.MEDIA_ROOT, 'pdfs', f"{filename}.pdf")
+    
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        raise Http404("File not found")
+
+    # Return a response to display the PDF in the browser
+    return FileResponse(open(file_path, 'rb'), content_type='application/pdf')
+
+# def serve_pdf(request, filename):
+#     # Define the path where the PDF files are stored
+#     # file_path = os.path.join('myapp', 'static', 'myapp', 'pdfs', f"{filename}.pdf")
+
+#     #if request.path.endswith('/'):
+#     #    return HttpResponseRedirect(request.path.rstrip('/'))
+
+#     file_path = finders.find(f"myapp/static/myapp/pdfs/{filename}") # !! filename or filename.pdf ?
+
+#     # Check if the file exists and serve it; raise 404 if it doesn’t
+#     if os.path.exists(file_path):
+#         return FileResponse(open(file_path, 'rb'), content_type='application/pdf')
+#     else:
+#         raise Http404("PDF file not found.")
